adb6a393986619c19191ed742edb513040d6ff84
Sohil876 <sohil876@gmail.com>
2020-07-07 19:24:39 +0530

    Bliss adaptation, nuked dependencies file

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tissot.mk
+    $(LOCAL_DIR)/bliss_tissot.mk
+
+COMMON_LUNCH_CHOICES := \
+    bliss_tissot-user \
+    bliss_tissot-userdebug \
+    bliss_tissot-eng \
