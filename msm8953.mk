d43606bf1ccd04e19ad5c859300672688620b962
Sohil876 <sohil876@gmail.com>
2020-12-12 20:05:47 +0530

    Nuke tetheroffload config/control

--- msm8953.mk
+++ msm8953.mk
@@ -399,9 +399,7 @@ PRODUCT_PACKAGES += \
     dhcpcd.conf \
     wificond \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    android.hardware.tetheroffload.config@1.0 \
-    android.hardware.tetheroffload.control@1.0
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
